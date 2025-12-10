4eb709b6d319fd5dc2a3ac60048d60349f30439f
EvMossan <cryptossan@gmail.com>
2025-12-10 13:22:20 +0200

    new file:   pyproject.toml 	new file:   src/cold_harbour_manager.egg-info/PKG-INFO 	new file:   src/cold_harbour_manager.egg-info/SOURCES.txt 	new file:   src/cold_harbour_manager.egg-info/dependency_links.txt 	new file:   src/cold_harbour_manager.egg-info/top_level.txt 	new file:   src/cold_harbour_manager/__init__.py 	new file:   src/cold_harbour_manager/services/__init__.py 	new file:   src/cold_harbour_web.egg-info/PKG-INFO 	new file:   src/cold_harbour_web.egg-info/SOURCES.txt 	new file:   src/cold_harbour_web.egg-info/dependency_links.txt 	new file:   src/cold_harbour_web.egg-info/top_level.txt 	modified:   src/coldharbor_manager/services/risks_manager.py

---
+++ src/cold_harbour_manager/__init__.py
@@ -0,0 +1,5 @@
+"""Snake-case alias for :mod:`coldharbor_manager`."""
+
+from coldharbor_manager import services  # noqa: F401
+
+__all__ = ["services"]
---
+++ src/cold_harbour_manager/services/__init__.py
@@ -0,0 +1,5 @@
+"""Alias for :mod:`coldharbor_manager.services`."""
+
+from coldharbor_manager.services.risks_manager import RiskManager  # noqa: F401
+
+__all__ = ["RiskManager"]
