47db8bd76ac5d09e1883de948f01bc826822f871
EvMossan <cryptossan@gmail.com>
2025-12-13 09:59:29 +0200

    deleted:    src/cold_harbour_manager/__init__.py 	deleted:    src/cold_harbour_manager/services/__init__.py

--- src/cold_harbour_manager/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Alias package that re-exports coldharbour_manager."""
-
-from coldharbour_manager import ACCOUNT_BLUEPRINTS, accounts_bp, create_app
-
-__all__ = ["create_app", "accounts_bp", "ACCOUNT_BLUEPRINTS"]
--- src/cold_harbour_manager/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Alias to expose services via cold_harbour_manager namespace."""
-
-from coldharbour_manager.services.risks_manager.risks_manager import (
-    BreakevenOrderManager,
-)
-
-__all__ = ["BreakevenOrderManager"]
